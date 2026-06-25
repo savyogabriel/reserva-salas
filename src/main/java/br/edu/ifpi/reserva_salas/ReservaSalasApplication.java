df76f1b6b101832e9d239b7a69b83bc669cd1a1a
savyo <savyogabriel24@gmail.com>
2026-06-24 22:58:29 -0300

    spring intializr

---
+++ src/main/java/br/edu/ifpi/reserva_salas/ReservaSalasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.reserva_salas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaSalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpi/reserva_salas/ReservaSalasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.reserva_salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaSalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
